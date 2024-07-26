5fe7bba2f4e288d90e54fbd96ab41fa73b87f472
boce.wang <boce.wang@zstack.io>
2024-07-26 16:42:56 +0800

    update module

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,23 @@
 use axum::{
-    extract::{Json, Query, State},
     routing::{get, post},
     Router,
 };
 use config::{Config, File};
 use env_logger::Builder;
-use log::{error, info, LevelFilter};
-use rdkafka::consumer::{Consumer, StreamConsumer};
-use rdkafka::producer::{FutureProducer, FutureRecord};
-use rdkafka::{ClientConfig, Message};
-use serde::{Deserialize, Serialize};
-use std::sync::Arc;
-use tokio_stream::StreamExt;
+use log::{info, LevelFilter};
+use serde::Deserialize;
 
 mod db;
-use db::{DataChangeEvent, DB};
+mod handlers;
+mod kafka;
+use db::DB;
+use handlers::{consume_events, receive_handler, send_handler, AppState};
+use kafka::{create_consumer, create_producer};
 
 #[derive(Debug, Deserialize)]
 struct WebConfig {
     listen_address: String,
     listen_port: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct KafkaConfig {
-    brokers: String,
-    topic: String,
-    group_id: String,
-    message_timeout_ms: String,
-    auto_offset_reset: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -43,21 +32,8 @@ struct DatabaseConfig {
 #[derive(Debug, Deserialize)]
 struct AppConfig {
     web: WebConfig,
-    kafka: KafkaConfig,
+    kafka: kafka::KafkaConfig,
     database: DatabaseConfig,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Pagination {
-    page: usize,
-    per_page: usize,
-}
-
-#[derive(Clone)]
-struct AppState {
-    producer: FutureProducer,
-    consumer: Arc<StreamConsumer>,
-    db: DB,
 }
 
 #[tokio::main]
@@ -73,29 +49,11 @@ async fn main() {
 
     let config: AppConfig = settings.try_deserialize().unwrap();
 
-    let kafka_brokers = &config.kafka.brokers;
-    let topic = &config.kafka.topic;
-    let group_id = &config.kafka.group_id;
-    let message_timeout_ms = &config.kafka.message_timeout_ms;
-    let auto_offset_reset = &config.kafka.auto_offset_reset;
-    let topics = [topic.as_str()];
+    let topic: &str = &config.kafka.topic;
+    let topics = &[topic];
 
-    let producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", kafka_brokers)
-        .set("message.timeout.ms", message_timeout_ms)
-        .create()
-        .expect("Producer creation error");
-
-    let consumer: StreamConsumer = ClientConfig::new()
-        .set("bootstrap.servers", kafka_brokers)
-        .set("group.id", group_id)
-        .set("auto.offset.reset", auto_offset_reset)
-        .create()
-        .expect("Consumer creation failed");
-
-    consumer
-        .subscribe(&topics)
-        .expect("Failed to subscribe to topics");
+    let producer = create_producer(&config.kafka);
+    let consumer = create_consumer(&config.kafka, topics);
 
     let db = DB::new(
         &config.database.dbname,
@@ -108,7 +66,7 @@ async fn main() {
 
     let state = AppState {
         producer,
-        consumer: Arc::new(consumer),
+        consumer,
         db,
     };
 
@@ -126,66 +84,5 @@ async fn main() {
     tokio::select! {
         _ = axum::serve(listener, app) => (),
         _ = consume_events(state) => (),
-    }
-}
-
-async fn send_handler(
-    State(state): State<AppState>,
-    Json(event): Json<DataChangeEvent>,
-) -> Result<Json<&'static str>, String> {
-    let payload = serde_json::to_string(&event).unwrap();
-    let record = FutureRecord::to("data_changes")
-        .payload(&payload)
-        .key(&event.key);
-
-    match state
-        .producer
-        .send(record, std::time::Duration::from_secs(0))
-        .await
-    {
-        Ok(_) => {
-            info!("Sent data change event to Kafka: {:?}", event);
-            Ok(Json("Message sent successfully"))
-        }
-        Err((e, _)) => {
-            error!("Failed to send event to Kafka: {:?}", e);
-            Err(format!("Failed to send message: {:?}", e))
-        }
-    }
-}
-
-async fn receive_handler(
-    State(state): State<AppState>,
-    Query(pagination): Query<Pagination>,
-) -> Result<Json<Vec<DataChangeEvent>>, String> {
-    let events = state
-        .db
-        .get_events(
-            pagination.per_page as i64,
-            (pagination.page * pagination.per_page) as i64,
-        )
-        .await?;
-    Ok(Json(events))
-}
-
-async fn consume_events(state: AppState) {
-    let mut stream = state.consumer.stream();
-    while let Some(result) = stream.next().await {
-        match result {
-            Ok(borrowed_message) => {
-                if let Some(payload) = borrowed_message
-                    .payload()
-                    .and_then(|p| std::str::from_utf8(p).ok())
-                {
-                    let event: DataChangeEvent = serde_json::from_str(payload).unwrap();
-                    info!("Received data change event: {:?}", event);
-
-                    if let Err(e) = state.db.insert_event(&event).await {
-                        error!("Failed to insert event into DB: {:?}", e);
-                    }
-                }
-            }
-            Err(e) => error!("Kafka error: {}", e),
-        }
     }
 }
